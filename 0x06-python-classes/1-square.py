ad73cbaa7c7cae3d843cc30442cc0c23d99e8839
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-05-31 01:45:29 +0300

    initial commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Write a class Square that defines a square by: (based on 0-square.py)
+
+-Private instance attribute: size
+-Instantiation with size (no type/value verification)
+-You are not allowed to import any module
+"""
+
+class Square:
+
+    """__init__ method used to initialize variables"""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
