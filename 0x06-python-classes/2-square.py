cafba54dc30b4e404ac5ad62556f3106e10b85f6
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-05-31 02:05:13 +0300

    alter word count

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,22 +4,15 @@
 
 
 class Square:
-    """Represent a square."""
+    """Declare square."""
 
     def __init__(self, size=0):
-        """Initialize a new Square.
-
+        """Instantiate a new Square.
         Args:
             size (int): The size of the new square.
-                It defaults to 0 if not provided.
-
-        Raises:
-            TypeError: If the size is not an integer.
-            ValueError: If the size is less than 0.
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("must be an integer")
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("must be >= 0")
         self.__size = size
-
