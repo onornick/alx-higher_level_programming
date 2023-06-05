4a5c8293eaa57128c42d1c7e57e07221d18ff035
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-06-05 22:17:56 +0300

    reviewed pycodestyle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+This module defines a Rectangle class that is empty
+"""
+
 
 class Rectangle:
-    """ Defines the blueprint of the Rectangle"""
+    """ 
+    Defines the blueprint of the Rectangle
+    """
     pass
