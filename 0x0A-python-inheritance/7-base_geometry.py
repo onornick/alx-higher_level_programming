0ab1b84cd252aff61a0b75a8bbf780151fb268a4
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-06-13 01:48:23 +0300

    initial commit

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""this module has one function area()"""
+
+
+class BaseGeometry:
+    """implements area() function"""
+    def area(self):
+        """area() implementation"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates input
+        Args:
+            name (str): assumed always a string
+            value (int): greater than 0
+        """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
