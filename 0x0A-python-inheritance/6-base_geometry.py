27d80429cdb40841bf0db4bef657c184c302fc8b
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-06-13 01:27:30 +0300

    initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this module has one function area()"""
+
+
+class BaseGeometry:
+    """implements area() function"""
+    def area(self):
+        """area() implementation"""
+        raise Exception("area() is not implemented")
