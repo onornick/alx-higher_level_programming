e121b4cdeb90e541266bbfa2bdf73fe6f7a75914
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-06-13 00:18:31 +0300

    initial commit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module inherits from the list"""
+
+
+class MyList(list):
+    """This class inherits from list"""
+
+
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
+        print(sorted(self))
