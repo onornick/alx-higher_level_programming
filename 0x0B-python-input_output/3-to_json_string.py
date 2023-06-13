9a068b490a0286945975256e68d6877cb4a25d72
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-06-14 00:04:38 +0300

    refactored documentation

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 """
 Module 3-to_json_string
 Contains function that returns JSON representation of obj (string)
@@ -6,6 +7,11 @@ Contains function that returns JSON representation of obj (string)
 
 
 def to_json_string(my_obj):
-    import json
-    """returns json string representation"""
+    """returns json string representation
+
+    Args:
+        my_obj: object
+
+    Raises:
+        Exception: when encoding can't take place"""
     return json.dumps(my_obj)
