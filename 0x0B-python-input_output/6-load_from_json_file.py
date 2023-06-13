bc837d2ff3f16d3830d73fa02fcd37ca6298bc4b
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-06-14 00:57:04 +0300

    initial commit

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""that creates an Object from a “JSON file”"""
+import json
+
+
+def save_to_json_file(filename):
+    """
+    a function that creates an object from
+    a json file
+
+    Args:
+        filename: filename
+
+    Raises:
+        Exception: when the object can't be encoded
+
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
