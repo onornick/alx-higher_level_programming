7720f85e6a5e784b9ad382e4cddd1b0c2e34b800
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-06-13 22:45:56 +0300

    add end=''

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """This function reads a txt file and prints to stdout"""
     with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
