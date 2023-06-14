077d1be150a9726f7c812d4e3c534bb8b28c2db3
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-06-14 16:43:51 +0300

    initial commit

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This module defines a class Student"""
+
+
+class Student:
+    """This class defines to_json function"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        This function returns a dict representation of
+        object instance
+
+        Args:
+            first_name: str
+            last_name: str
+            age: int
+
+        Method:
+            to_json: returns a dict rep of the instance
+
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        value = {}
+        if attrs is None:
+            return self.__dict__
+        for k in attrs:
+            if k in self.__dict__.keys():
+                value[k] = self.__dict__[k]
+        return value
