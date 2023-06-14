af40eaaf9aad4116b1a303d4b7f50164abb1fd71
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-06-14 14:36:06 +0300

    initial commit

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that returns the dictionary description"""
+
+def class_to_json(obj):
+    """ Function that retuns the dictionary description of an obj """
+
+    if hasattr(obj, "__dict__"):
+        return obj.__dict__
