8b953686ebafa570e5d21387174877310c6a0427
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-06-20 17:39:11 +0300

    removed blank line at base.py end

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -78,15 +78,16 @@ class Base():
     def load_from_file(cls):
         """Returns list of instances"""
         filenamee = cls.__name__ + ".json"
-        l = []
+        instance_list = []
         try:
             with open(filenamee, "r") as f:
                 instances = cls.from_json_string(f.read())
             for i, dic in enumerate(instances):
-                l.append(cls.create(**instances[i]))
-        except:
-            pass
-        return l
+                instance_list.append(cls.create(**instances[i]))
+        except FileNotFoundError:
+            return []
+
+        return instance_list
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
@@ -120,4 +121,3 @@ class Base():
                 o = cls.create(**dic)
                 objs.append(o)
         return
- 
