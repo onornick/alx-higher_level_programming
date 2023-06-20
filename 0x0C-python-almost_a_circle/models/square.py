45fe9e253a4ec4529ffb95316f8fa46e660c929d
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-06-20 17:47:30 +0300

    added test cases for square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -39,21 +39,24 @@ class Square(Rectangle):
         to_dictionary(self)
     """
     def __init__(self, size, x=0, y=0, id=None):
+        """Initialize"""
         super().__init__(size, size, x, y, id)
         self.size = size
 
     @property
     def size(self):
+        """Getter size"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """Setter size - sets width and height as size"""
         self.width = value
         self.height = value
 
     def __str__(self):
         """Prints [Square] (<id>) <x>/<y> - <size>"""
-        return "{:s} ({:d}) {:d}/{:d} - {:d}".format(
+        return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
             self.__class__.__name__, self.id, self.x, self.y,
             self.size)
 
@@ -63,18 +66,24 @@ class Square(Rectangle):
         If no args given: set attributes according to kwargs
         """
         if args:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.size = args[1]
-            if len(args) >= 3:
-                self.x = args[2]
-            if len(args) >= 4:
-                self.y = args[3]
-
-        if kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+            for k, v in enumerate(args):
+                if k == 0:
+                    self.id = v
+                elif k == 1:
+                    self.size = v
+                elif k == 2:
+                    self.x = v
+                else:
+                    self.y = v
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
 
     def to_dictionary(self):
         """Return dictionary representation"""
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -8,59 +8,120 @@ Unittest for Square Class
 
 import unittest
 import pep8
-from models.square import Square
+from io import StringIO
+from contextlib import redirect_stdout
+from models import square
+Square = square.Square
 
 
-class TestSquare(unittest.TestCase):
+class TestPep8(unittest.TestCase):
+    """Pep8 models/square.py & tests/test_models/test_square.py"""
+    def test_pep8(self):
+        """Pep8"""
+        style = pep8.StyleGuide(quiet=False)
+        errors = 0
+        files = ["models/square.py", "tests/test_models/test_square.py"]
+        errors += style.check_files(files).total_errors
+        self.assertEqual(errors, 0, 'Need to fix Pep8')
 
-    def test_init(self):
-        s = Square(5, 1, 1, 1)
-        self.assertEqual(s.size, 5)
-        self.assertEqual(s.x, 1)
-        self.assertEqual(s.y, 1)
-        self.assertEqual(s.id, 1)
 
-    def test_size(self):
-        s = Square(5)
-        self.assertEqual(s.size, 5)
-        s.size = 10
-        self.assertEqual(s.size, 10)
+class TestBase(unittest.TestCase):
+    """Tests for models/square.py"""
+
+    """Test attributes"""
+    def test_all_attr_given(self):
+        """Test all attributes match what's given"""
+        s1 = Square(9, 99, 999, 1000)
+        self.assertTrue(s1.width == 9)
+        self.assertTrue(s1.height == 9)
+        self.assertTrue(s1.size == 9)
+        self.assertTrue(s1.x == 99)
+        self.assertTrue(s1.y == 999)
+        self.assertTrue(s1.id == 1000)
+
+    def test_default_attr(self):
+        """Test default attributes are set when not given"""
+        s2 = Square(88)
+        self.assertTrue(s2.width == 88)
+        self.assertTrue(s2.height == 88)
+        self.assertTrue(s2.size == 88)
+        self.assertTrue(s2.x == 0)
+        self.assertTrue(s2.y == 0)
+        self.assertTrue(s2.id is not None)
+
+    def test_attr_validated(self):
+        """Test attributes are validated before set"""
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Square("10")
+            Square([10, 3])
+            Square({20, })
+            Square({"d": 20})
+            Square(None)
+            Square((30, 20), 4)
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Square(-1)
+            Square(9).size(-9)
+
+    """Test args given"""
+    def test_invalid_args(self):
+        """Test too many args given throws error"""
         with self.assertRaises(TypeError):
-            s.size = "10"
-        with self.assertRaises(ValueError):
-            s.size = -10
+            Square(1, 2, 3, 4, 5, 6, 7)
+        """Test too little args given throws error"""
+        with self.assertRaises(TypeError):
+            Square()
+            Square(None)
 
-    def test_str(self):
-        s = Square(5, 1, 1, 1)
-        expected_output = "Square (1) 1/1 - 5"
-        self.assertEqual(str(s), expected_output)
+    """Test class"""
+    def test_class(self):
+        """Test class created is indeed Rectangle"""
+        s = Square(10)
+        self.assertEqual(type(s), Square)
+
+    """Test methods"""
+    def test_area(self):
+        """Test method: area"""
+        self.assertEqual(Square(3).area(), 9)
+        self.assertEqual(Square(4, 0, 0).area(), 16)
+
+    def test_display(self):
+        """Test method: display"""
+        with StringIO() as bufr, redirect_stdout(bufr):
+            Square(4).display()
+            b = bufr.getvalue()
+        self.assertEqual(b, '####\n####\n####\n####\n')
+        with StringIO() as bufr, redirect_stdout(bufr):
+            Square(3, 1, 2).display()
+            b = bufr.getvalue()
+        self.assertEqual(b, '\n\n ###\n ###\n ###\n')
+
+    def test_print(self):
+        """Test method: __str__"""
+        s = Square(1, 2, 3, 44)
+        s.size = 500
+        self.assertEqual(str(s), '[Square] (44) 2/3 - 500')
 
     def test_update(self):
-        s = Square(5, 1, 1, 1)
-        s.update(2, 10, 2, 2)
-        self.assertEqual(s.id, 2)
-        self.assertEqual(s.size, 10)
-        self.assertEqual(s.x, 2)
-        self.assertEqual(s.y, 2)
-
-        s.update(size=15, x=3, y=3)
-        self.assertEqual(s.size, 15)
-        self.assertEqual(s.x, 3)
-        self.assertEqual(s.y, 3)
+        """Test method: update(*args)"""
+        s = Square(1, 2, 3, 4)
+        s.update(10, 10, 10, 10)
+        self.assertEqual(str(s), '[Square] (10) 10/10 - 10')
+        s.update()
+        self.assertEqual(str(s), '[Square] (10) 10/10 - 10')
+        s.update(99)
+        self.assertEqual(str(s), '[Square] (99) 10/10 - 10')
+        s.update(99, 5)
+        self.assertEqual(str(s), '[Square] (99) 10/10 - 5')
+        s.update(44, 55, 1, 2)
+        self.assertEqual(str(s), '[Square] (44) 1/2 - 55')
+        """Test method: update(*kwargs)"""
+        s.update(id=88, size=77, nokey=99)
+        self.assertEqual(str(s), '[Square] (88) 1/2 - 77')
 
     def test_to_dictionary(self):
-        s = Square(5, 1, 1, 1)
-        expected_output = {'id': 1, 'size': 5, 'x': 1, 'y': 1}
-        self.assertEqual(s.to_dictionary(), expected_output)
-
-    def test_pep8_conformance(self):
-        """Test that the code conforms to PEP8."""
-        style = pep8.StyleGuide()
-        result = style.check_files(['models/square.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-
-if __name__ == "__main__":
-    unittest.main()
-
+        """Test method: to_dictionary"""
+        sdic = Square(1, 2, 3, 4).to_dictionary()
+        self.assertEqual(type(sdic), dict)
+        s2 = Square(10, 10)
+        s2.update(**sdic)
+        self.assertEqual(str(s2), '[Square] (4) 2/3 - 1')
