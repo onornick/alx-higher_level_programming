42f984cb1b2c9f0e60db4bb51ca87d0572b18bce
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-07-21 16:22:45 +0300

    initial commit

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- creates the database hbtn_0d_usa and the table states
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+				id INT PRIMARY KEY NOT NULL UNIQUE AUTO INCREMENT, 
+				name VARCHAR(256) NOT NULL);
