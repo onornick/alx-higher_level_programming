9aada97e9485dd5c3d70cdbbed49031528e74af2
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-07-21 23:24:10 +0300

    Added group by id clause

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,5 @@
 -- lists all the cities of Carlifonia in the db
 
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'Carlifonia') GROUP BY id ORDER BY id ASC;
+SELECT id, name
+  FROM cities
+ WHERE state_id = (SELECT id FROM states WHERE name = "California") GROUP BY id ORDER BY id ASC;
