ee503020762901bcecfa90a174bf39b8e0cce1b5
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-07-21 18:03:25 +0300

    remove extra space within querry

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- lists all the cities of Carlifonia in the db
 
-SELECT id, name  FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'Carlifonia') ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'Carlifonia') ORDER BY id ASC;
