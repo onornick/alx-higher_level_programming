03dad5741bba96fb36d256b3f0a597c068a59cf7
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-07-22 00:04:48 +0300

    Placed each statement on it's row

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,6 @@
 -- lists all cities contained in the database hbtn_0d_usa
 
-SELECT cities.id, cities.name, states.name FROM cities JOIN states WHERE cities.state_id = state.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = state.id
+ORDER BY cities.id ASC;
