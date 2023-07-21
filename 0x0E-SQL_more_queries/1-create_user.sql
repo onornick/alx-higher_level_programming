bcf652f2c16917b6221c5617aab49c9110a9577e
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-07-21 13:23:30 +0300

    changed permissions to *.*

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates mysql user user_0d_1
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
-GRANT ALL ON localhost.* TO 'user_0d_1'@'localhost'
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost'
