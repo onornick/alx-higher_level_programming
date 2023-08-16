ba314cc4e61e65e8bd6ea52adaa47559cd1450ea
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-08-17 02:22:29 +0300

    reviewed semistandard code

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const myList = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-i = 0;
-while(i < myList.length){
-    console.log(myList[i]);
-    i++;
+let i = 0;
+while (i < myList.length) {
+  console.log(myList[i]);
+  i++;
 }
