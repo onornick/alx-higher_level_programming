c485d5372bfdfc27b32c6b72542bd05bbe824b10
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-08-23 18:11:51 +0300

    initial commit

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', port='3306', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     curs = db.cursor()
 
     curs.execute("SELECT * FROM state WHERE name LIKE 'N%' ORDER BY id")
