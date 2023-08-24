961688a690b4ab1764c963bdfdee2221e224aaca
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-08-24 15:43:48 +0300

    added comments

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+"""
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
     state_name = sys.argv[4]
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     db_cursor = db.cursor()
