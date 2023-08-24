e1110bea7eb79bbc1ce3c59e2da604c21a5dfb6f
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-08-24 14:01:42 +0300

    initial commit

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    state_name = sys.argv[4]
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db_cursor = db.cursor()
+    db_cursor.execute("SELECT * FROM states WHERE name = %s", (state_name,))
+    rows = db_cursor.fetchall()
+
+    for row in rows:
+        print(rows)
+
+    db_cursor.close()
+    db.close()
