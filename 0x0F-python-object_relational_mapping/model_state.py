9851b5739d9eff70a1a4bd9653b6a1eb98a8ac37
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-08-27 23:20:46 +0300

    pycodestyle review

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative
+"""
+Defines a state model that contain the class definition
+ of a State and an instance Base = declarative_base()
+"""
+from lib2to3.pytree import Base
+from sre_parse import State
+from unicodedata import name
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class Users(Base):
-    __tablename__ = 'users'
 
+class State(Base):
+    """
+    inherits from Base Tips
+    links to the MySQL table states
+    class attribute id that represents a column
+     of an auto-generated, unique integer, can't
+      be null and is a primary key
+    class attribute name that represents a column
+     of a string with maximum 128 characters and
+      can't be null
+    """
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True)
-    name = Column(String(15))
-    fullname = Column(String(20))
-    nickname = Column(String)
-
-    def __repr__(self):
-        return "<User(name='%s', fullname='%s', nickname='%s')>"\
-            %(self.name, self.fullname, self.nickname)
-
-    def __str__(self):
-        print(Users.__table__)
-
-    
+    name = Column(String(128), nullable=False)
