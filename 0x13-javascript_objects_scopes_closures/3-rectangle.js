a6e9a9cb6be18995fd9d6069d39119d8d191f9da
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-09-03 17:07:06 +0300

    change the pattern to print X instead of #

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -21,7 +21,7 @@ class Rectangle {
     for (let i = 0; i < this.height; i++) {
       let row = '';
       for (let j = 0; j < this.width; j++) {
-        row += '#';
+        row += 'X';
       }
       console.log(row);
     }
