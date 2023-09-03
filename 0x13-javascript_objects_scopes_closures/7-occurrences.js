59bb8e994e89342cdaf77be334abf5b139cf9ca7
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-09-03 23:08:12 +0300

    changed the function to an anonymous function

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,17 +3,14 @@
  * function that returns the number of occurrences in a list:
  * Prototype: exports.nbOccurences = function (list, searchElement)
  */
-function nbOccurences (list, searchElement) {
+const nbOccurences = function (list, searchElement) {
   let counter = 0;
   for (let i = 0; i < list.length; i++) {
-    if (list[i] == searchElement) {
+    if (list[i] === searchElement) {
       counter++;
     }
   }
   return counter;
-}
+};
 
 module.exports = nbOccurences;
-console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
-console.log(nbOccurences([3, 2, 3, 4, 5, 3, 3], 3));
-console.log(nbOccurences(["S", 12, "c", "S", "School", 8], "S"));
