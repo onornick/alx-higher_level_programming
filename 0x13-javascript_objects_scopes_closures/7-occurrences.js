7057fc8d557a4cd59648bbf96dab6b1cd5585094
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-09-03 22:54:08 +0300

    change the === to ==

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -6,7 +6,7 @@
 function nbOccurences (list, searchElement) {
   let counter = 0;
   for (let i = 0; i < list.length; i++) {
-    if (list[i] === searchElement) {
+    if (list[i] == searchElement) {
       counter++;
     }
   }
@@ -14,3 +14,6 @@ function nbOccurences (list, searchElement) {
 }
 
 module.exports = nbOccurences;
+console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
+console.log(nbOccurences([3, 2, 3, 4, 5, 3, 3], 3));
+console.log(nbOccurences(["S", 12, "c", "S", "School", 8], "S"));
