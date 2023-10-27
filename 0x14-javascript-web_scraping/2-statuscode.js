5fb7fb487142de002798bde25e026ea6c9ff7022
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-10-27 12:33:34 +0300

    initial commit

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length < 3) {
+  console.log('Usage: ./2-statuscode.js [url]');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+request(url, function (err, response) {
+  if (err) {
+    console.log('Error: ', err);
+  }
+
+  console.log('code: ', response.statusCode);
+});
